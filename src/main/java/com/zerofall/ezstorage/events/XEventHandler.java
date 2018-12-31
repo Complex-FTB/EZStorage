4643117f94ddd070cf43742f20a54cfff5d2d6b4
Petrus <petrus_ant@hotmail.com>
2018-12-31 05:51:36 -0200

    Prevent player's from placing core instead of just warning them.

--- src/main/java/com/zerofall/ezstorage/events/XEventHandler.java
+++ src/main/java/com/zerofall/ezstorage/events/XEventHandler.java
@@ -2,6 +2,7 @@ package com.zerofall.ezstorage.events;
 
 import com.zerofall.ezstorage.tileentity.TileEntityStorageCore;
 
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 
 public class XEventHandler {
@@ -13,6 +14,19 @@ public class XEventHandler {
 				final TileEntityStorageCore core = (TileEntityStorageCore) tileentity;
 				if (core.inventory.getTotalCount()>0L)
 					e.setCanceled(true);
+			}
+		}
+	}
+
+	@cpw.mods.fml.common.eventhandler.SubscribeEvent
+	public void onBlockBreak(final BlockEvent.PlaceEvent e) {
+		if (!e.world.isRemote) {
+			final net.minecraft.tileentity.TileEntity tileentity = e.world.getTileEntity(e.x, e.y, e.z);
+			if (tileentity instanceof TileEntityStorageCore) {
+				final TileEntityStorageCore core = (TileEntityStorageCore) tileentity;
+				if (!core.validateSystem(null) && core.inventory.getTotalCount()==0L){
+					e.setCanceled(true);
+				}
 			}
 		}
 	}
